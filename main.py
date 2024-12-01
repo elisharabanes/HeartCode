84f187cbaba83b256985d45398809f3c454edba8
elisharabanes <elishaarabanes@gmail.com>
2024-12-01 21:53:04 +0000

    added tests, printed output

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ def isAnagram(word1, word2):
   sortWord1 = sorted(word1) #sorts word1 into alphabethical order
   sortWord2 = sorted(word2) # sorts word2 alphabet order
 
-  return sortWord1 == sortWord2 #checks if they are the same
+   #checks if they are the same and returns whether or not is an   anagram
+  if (sortWord1 == sortWord2): 
+    print("True")
+  else:
+    print("False")
+# test 1 
+isAnagram('silent', 'listen')
+# test 2 
+isAnagram('maemae', 'elisha')
